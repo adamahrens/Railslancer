4eae314550dae21bcf04aa4540bdc58e61545471
Adam Ahrens <adam.aahrens@gmail.com>
2019-02-23 16:23:06 -0600

    Tweaking initial stuff in app component

--- freelance-frontend/src/app/app.component.spec.ts
+++ freelance-frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'freelance-frontend'`, () => {
+  it(`should have as title 'Freelancer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('freelance-frontend');
+    expect(app.title).toEqual('Freelancer');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to freelance-frontend!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Freelancer!');
   });
 });
