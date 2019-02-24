1bbf68dc7952d8b1aec9babb15e1d05c810dd446
Adam Ahrens <adam.aahrens@gmail.com>
2019-02-23 18:11:16 -0600

    Adding documents component

--- freelance-frontend/src/app/app-routing.module.ts
+++ freelance-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DocumentsComponent } from './documents/documents.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'documents', component: DocumentsComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- freelance-frontend/src/app/app.module.ts
+++ freelance-frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DocumentsComponent } from './documents/documents.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocumentsComponent
   ],
   imports: [
     BrowserModule,
