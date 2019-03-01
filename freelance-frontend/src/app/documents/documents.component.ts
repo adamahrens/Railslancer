1b45a732c7cb1aaf4ec4353da322da5a7acc4099
Adam Ahrens <adam.aahrens@gmail.com>
2019-02-28 20:29:33 -0600

    Tweaking styling for Documents and Proposals. Adding dummy document data to display

--- freelance-frontend/src/app/documents/documents.component.ts
+++ freelance-frontend/src/app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Document } from "./document"
 
 @Component({
   selector: 'documents',
@@ -8,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 
 export class DocumentsComponent implements OnInit {
 
+  documents: Document[]
+
   constructor() { }
 
   ngOnInit() {
+    this.documents = [
+      {title: 'My Document', description: 'A sample document to use', file_url: '', updated_at: '1/28/2019', image_url: 'https://via.placeholder.com/150'},
+      {title: 'W2 Form', description: 'A W2 form that should be filled out', file_url: '', updated_at: '1/28/2019', image_url: 'https://via.placeholder.com/150'},
+      {title: 'Reimbursement Form', description: 'Use when travelling to expense items', file_url: '', updated_at: '1/28/2019', image_url: 'https://via.placeholder.com/150'},
+      {title: 'Syllabus', description: 'Course syllabus to follow', file_url: '', updated_at: '1/28/2019', image_url: 'https://via.placeholder.com/150'},
+      {title: 'Code of Conduct', description: 'A ethical handbook for navigating the business world', file_url: '', updated_at: '1/28/2019', image_url: 'https://via.placeholder.com/150'},
+      {title: 'Health Directive', description: 'Health directive to follow.', file_url: '', updated_at: '1/28/2019', image_url: 'https://via.placeholder.com/150'}
+    ]
   }
-
 }
