5f3639c01f7ad18785c75c8afbf8743d6a79717f
Adam Ahrens <adam.aahrens@gmail.com>
2019-03-01 20:10:36 -0600

    Displaying dummy Proposal data

--- freelance-frontend/src/app/proposals/proposals.component.ts
+++ freelance-frontend/src/app/proposals/proposals.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Proposal } from "./proposal"
 
 @Component({
   selector: 'app-proposals',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProposalsComponent implements OnInit {
 
+  proposals: Proposal[]
+
   constructor() { }
 
   ngOnInit() {
+    this.proposals = [
+      new Proposal(1, 'When I Work', 'http://www.wheniwork.com', 'PHP React Go Swift', 40, 41, 36, 'support@wheniwork.com'),
+      new Proposal(2, 'Appsbyahrens', 'http://www.appsbyahrens.com', 'Swift Objective-C Ruby on Rails', 40, 41, 36, 'support@appsbyahrens.com'),
+      new Proposal(3, 'SmartThings', 'http://www.smartthings.com', 'Swift Objective-C Kotlin', 40, 41, 36, 'support@smartthings.com'),
+      new Proposal(4, 'Thomson Reuters', 'http://www.thomsonreuters.com', 'C# Selenium', 40, 41, 36, 'support@reuters.com')
+    ]
   }
 
 }
